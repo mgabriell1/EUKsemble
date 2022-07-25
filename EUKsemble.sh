038940664a77c46f137c9e0e7b380ff6bc38fa27
Marco Gabrielli <marco.gabrielli@polimi.it>
2022-07-24 20:20:08 -0400

    Introduced the possibility to choose either CAT or Kaiju

--- EUKsemble.sh
+++ EUKsemble.sh
@@ -8,41 +8,49 @@ source $1
 eval "$(conda shell.bash hook)"
 
 ###### PREPARE FILES #####
-printf "\nClassfying assembly: $FOLDER$DATA$FILE_EXT \n\n"
+printf "\nClassifying assembly: $FOLDER$DATA$FILE_EXT \n\n"
 mkdir -p $FOLDER"/"$RESULTS_FOLDER
 
-if [ ! -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT ] && [ ! -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT ]; then
+
+if [ ${DB_CLASSIFIER,,} == "kaiju" ]; then
+	mkdir -p $KAIJURESULTS_FOLDER
+elif [ ${DB_CLASSIFIER,,} == "cat" ]; then
+	mkdir -p $CATRESULTS_FOLDER
+else
+	printf "Reference-based classification method not recognized. \n"
+	exit
+fi
+	
+
+if [ ! -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT ] && [ ! -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT ]; then
     printf "Prepare files for classification \n"
     conda activate $SEQKIT_ENV
     seqkit seq $FOLDER"/"$DATA$FILE_EXT -m $MINSIZE_CONTIGS_KMERCLASS -o $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT 
     grep ">" $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT | awk 'sub(/^>/, "")'  > $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp_contigsIDs.txt"
 
-    if [ $MINSIZE_CONTIGS_KMERCLASS != $MINSIZE_CONTIGS_KAIJUCLASS ]; then
-        seqkit seq $FOLDER"/"$DATA$FILE_EXT -m $MINSIZE_CONTIGS_KAIJUCLASS -o $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT
-        grep ">" $FOLDERv"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT | awk 'sub(/^>/, "")'  > $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp_contigsIDs.txt"
+    if [ $MINSIZE_CONTIGS_KMERCLASS != $MINSIZE_CONTIGS_DBCLASS ]; then
+        seqkit seq $FOLDER"/"$DATA$FILE_EXT -m $MINSIZE_CONTIGS_DBCLASS -o $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT
+        grep ">" $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT | awk 'sub(/^>/, "")'  > $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp_contigsIDs.txt"
     fi
-    
-	mkdir -p $KAIJURESULTS_FOLDER
 	
 	if [ $KAIJU_LOCAL != "TRUE" ]; then
-	    printf "Next steps for classification:\n 1. Perform Kaiju classification at https://kaiju.binf.ku.dk/server with the generated ${DATA}_min${MINSIZE_CONTIGS_KAIJUCLASS}bp${FILE_EXT} file (after compression) and default settings\n 2. Save Kaiju's output file in $KAIJURESULTS_FOLDER \n 3. Re-run the pipeline script for k-mer classification and final output\n"
+	    printf "Next steps for classification:\n 1. Perform Kaiju classification at https://kaiju.binf.ku.dk/server with the generated ${DATA}_min${MINSIZE_CONTIGS_DBCLASS}bp${FILE_EXT} file (after compression) and default settings\n 2. Save Kaiju's output file in $KAIJURESULTS_FOLDER \n 3. Re-run the pipeline script for k-mer classification and final output\n"
 	    exit
     fi
 fi
 
-#printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 printf "===============================================\n"
 
 ###### KAIJU CLASSIFICATIONS #####
 
-if [ $KAIJU_LOCAL == "TRUE" ]; then
+if [ $KAIJU_LOCAL == "TRUE" ] && [ ${DB_CLASSIFIER,,} == "kaiju" ]; then
 	printf "Kaiju classification \n"
 	
 	if [ ! -f $KAIJURESULTS_FOLDER"/"$KAIJURESULTS_FILENAME".taxa"$KAIJURESULTS_EXT ]; then
 		conda activate $KAIJU_ENV
 		kaiju -t $KAIJU_DB_NODES \
 			-f $KAIJU_DB_FMI \
-			-i $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT \
+			-i $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT \
 			-z $THREADS \
 			-o $KAIJURESULTS_FOLDER"/"$KAIJURESULTS_FILENAME$KAIJURESULTS_EXT \
 			-e $KAIJU_PARAMS_e -E $KAIJU_PARAMS_E -s $KAIJU_PARAMS_s -v
@@ -56,12 +64,36 @@ if [ $KAIJU_LOCAL == "TRUE" ]; then
 	fi
 fi
 
-#printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+###### CAT CLASSIFICATIONS #####
+
+if [ ${DB_CLASSIFIER,,} == "cat" ]; then
+	printf "CAT classification \n"
+	
+	conda activate $CAT_ENV
+	if [ ! -f $CATRESULTS_FOLDER"/"$CATRESULTS_FILENAME".contig2classification.official_names.txt" ]; then
+		CAT contigs -c $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT \
+			-d $CAT_DB \
+			-t $CAT_TAXONOMY \
+			-n $THREADS \
+			--path_to_diamond $CAT_DIAMOND_PATH \
+			--index_chunks 1 --verbose \
+			--out_prefix $CATRESULTS_FOLDER"/"$CATRESULTS_FILENAME
+			
+		CAT add_names -i $CATRESULTS_FOLDER"/"$CATRESULTS_FILENAME".contig2classification.txt" \
+			-t $CAT_TAXONOMY \
+			--only_official \
+			-o $CATRESULTS_FOLDER"/"$CATRESULTS_FILENAME".contig2classification.official_names.txt"
+				
+	else
+		printf "CAT classification already present. Skipped \n"
+	fi
+fi
+
 printf "===============================================\n"
 
 ###### K-MER CLASSIFICATIONS #####
 
-source activate $WHOKARYOTE_ENV #Definition from whokaryote github
+conda activate $WHOKARYOTE_ENV #Definition from whokaryote github
 
 printf "Whokaryote classification \n"
 if [ ! -d $FOLDER"/"$RESULTS_FOLDER"/whokaryote-results_min"$MINSIZE_CONTIGS_KMERCLASS"bp" ]; then
@@ -71,7 +103,7 @@ if [ ! -d $FOLDER"/"$RESULTS_FOLDER"/whokaryote-results_min"$MINSIZE_CONTIGS_KME
 else
     printf "Whokaryote classification already present. Skipped \n"
 fi
-#printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+
 printf "===============================================\n"
 
 conda activate $TIARA_ENV 
@@ -87,13 +119,12 @@ else
     printf "Tiara classification already present. Skipped \n"
 fi
     
-#printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 printf "===============================================\n"
 
 conda activate $DEEPMICROBEFINDER_ENV #Definition from DeepMicrobeFinder github
 
 printf "DeepMicrobeFinder classification \n"
-if [ ! -d $FOLDER"/"$RESULTS_FOLDER"/deepmicrobefinder-results_min"$MINSIZE_CONTIGS_KMERCLASS"bp" ]; then
+if [ ! -f $FOLDER"/"$RESULTS_FOLDER"/deepmicrobefinder-results_min"$MINSIZE_CONTIGS_KMERCLASS"bp/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT"_pred_one-hot_hybrid.class.txt" ]; then
     python $DEEPMICROBEFINDER_FOLDER"/predict.py" \
         -i $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT  \
         -e one-hot \
@@ -106,7 +137,6 @@ else
     printf "DeepMicrobeFinder classification already present. Skipped \n"
 fi
 
-#printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 printf "===============================================\n"
 
 ##### ASSIGN TAXONOMY TO KAIJU WEBSERVER RESULTS #####
@@ -116,28 +146,28 @@ if [ $KAIJU_LOCAL != "TRUE" ]; then
 	    $KAIJURESULTS_FOLDER"/" \
 	    $KAIJURESULTS_FILENAME \
 	    $KAIJURESULTS_EXT \
-	    $MINSIZE_CONTIGS_KAIJUCLASS \
+	    $MINSIZE_CONTIGS_DBCLASS \
 	    $ACCESSIONTAXADB_TAXONOMIZR"/"
 	
-	%printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 	printf "===============================================\n"
 fi 
 ##### PERFORM CLASSIFICATION #####
 
 printf "Perform majority classification \n"
 $RSCRIPT_PATH $EUKSEMBLE_INSTALLATION_FOLDER"/Scripts/Majority_classification_extInput.R" \
-    $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp_contigsIDs.txt" \
+    $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp_contigsIDs.txt" \
     $FOLDER"/"$RESULTS_FOLDER"/whokaryote-results_min"$MINSIZE_CONTIGS_KMERCLASS"bp/featuretable_predictions_T.tsv" \
     $FOLDER"/"$RESULTS_FOLDER"/tiara-results_min"$MINSIZE_CONTIGS_KMERCLASS"bp/tiara-out_classification.txt" \
-    $FOLDER"/"$RESULTS_FOLDER"/deepmicrobefinder-results_min"$MINSIZE_CONTIGS_KMERCLASS"bp/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT"_pred_one-hot_hybrid.txt" \
+    $FOLDER"/"$RESULTS_FOLDER"/deepmicrobefinder-results_min"$MINSIZE_CONTIGS_KMERCLASS"bp/"$DATA"_min"$MINSIZE_CONTIGS_KMERCLASS"bp"$FILE_EXT"_pred_one-hot_hybrid.class.txt" \
     $KAIJURESULTS_FOLDER"/"$KAIJURESULTS_FILENAME".taxa"$KAIJURESULTS_EXT \
+    $CATRESULTS_FOLDER"/"$CATRESULTS_FILENAME".contig2classification.official_names.txt"\
     $FOLDER"/"$RESULTS_FOLDER"/" \
     $MINSIZE_CONTIGS_KMERCLASS \
-    $MINSIZE_CONTIGS_KAIJUCLASS \
+    $MINSIZE_CONTIGS_DBCLASS \
     $INCLUDE_NA \
+    $DB_CLASSIFIER \
     $KAIJU_LOCAL
 
-#printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 printf "===============================================\n"
 
 ##### EXTRACT EUKARYOTIC CONTIGS #####
@@ -145,7 +175,7 @@ printf "===============================================\n"
 printf "Extract eukaryotic contigs in new fasta file \n"
 conda activate $SEQKIT_ENV
 if [ $INCLUDE_NA == "TRUE" ]; then
-	seqkit grep -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_NA_contigsIDs_Kmer_min"$MINSIZE_CONTIGS_KMERCLASS"bp_Kaiju_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp.txt" $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT -o $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_NA_Kmer_min"$MINSIZE_CONTIGS_KMERCLASS"bp_Kaiju_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT
+	seqkit grep -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_NA_contigsIDs_KmerMajority_min"$MINSIZE_CONTIGS_KMERCLASS"bp_"${DB_CLASSIFIER^^}"_min"$MINSIZE_CONTIGS_DBCLASS"bp.txt" $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT -o $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_NA_KmerMajority_min"$MINSIZE_CONTIGS_KMERCLASS"bp_"${DB_CLASSIFIER^^}"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT
 else
-	seqkit grep -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_contigsIDs_Kmer_min"$MINSIZE_CONTIGS_KMERCLASS"bp_Kaiju_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp.txt" $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT -o $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_Kmer_min"$MINSIZE_CONTIGS_KMERCLASS"bp_Kaiju_min"$MINSIZE_CONTIGS_KAIJUCLASS"bp"$FILE_EXT
+	seqkit grep -f $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_contigsIDs_KmerMajority_min"$MINSIZE_CONTIGS_KMERCLASS"bp_"${DB_CLASSIFIER^^}"_min"$MINSIZE_CONTIGS_DBCLASS"bp.txt" $FOLDER"/"$RESULTS_FOLDER"/"$DATA"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT -o $FOLDER"/"$RESULTS_FOLDER"/"$DATA".EUK_KmerMajority_min"$MINSIZE_CONTIGS_KMERCLASS"bp_"${DB_CLASSIFIER^^}"_min"$MINSIZE_CONTIGS_DBCLASS"bp"$FILE_EXT
 fi
--- Scripts/Majority_classification_extInput.R
+++ Scripts/Majority_classification_extInput.R
@@ -14,18 +14,20 @@ whokaryote_classFile <- args[2]
 tiara_classFile <- args[3]
 deepmicrobefinder_classFile <- args[4]
 kaiju_classFile <- args[5]
-out_folder <- args[6]
-kmer_minLength<- args[7]
-kaiju_minLength <- args[8]
-include_na <- args[9]
-kaiju_local <- args[10]
+cat_classFile <- args[6]
+out_folder <- args[7]
+kmer_minLength<- args[8]
+refdb_minLength <- args[9]
+include_na <- args[10]
+refdb_classifier <- args[11]
+kaiju_local <- args[12]
 
 results_filename <- sub("*.*/", "", contigs_classFile)
 results_filename <- sub("_min.*", "", results_filename)
 
 ##### LOAD CONTIGS IDS #####
 
-contigs_class <- read_csv(contigs_classFile, col_names = FALSE) %>% rename(ContigID = X1)
+contigs_class <- read_tsv(contigs_classFile, col_names = FALSE) %>% rename(ContigID = X1)
 
 
 ##### LOAD CLASSIFICATION RESULTS #####
@@ -40,14 +42,20 @@ deepmicrobefinder_class <- read_delim(deepmicrobefinder_classFile, "\t", escape_
   rename(ContigID = 'Sequence Name', DeepMicrobeFinder_class = Prediction) %>%
   mutate(DeepMicrobeFinder_class = ifelse(DeepMicrobeFinder_class == "Eukaryote", "EUK", "OTHER"))
 
-if (kaiju_local == "TRUE"){
+if (tolower(refdb_classifier) == "kaiju"){
+	if (kaiju_local == "TRUE"){
 	kaiju_class <- read_delim(kaiju_classFile, delim = "\t", col_names = FALSE) %>%
 		rename(ContigID = X2) %>%
 		mutate(Kaiju_class = ifelse(str_detect(X8, "Eukaryota"), "EUK", "OTHER"))
-}else{
-	kaiju_class <- read_delim(kaiju_classFile, delim = "\t", escape_double = FALSE, col_names = TRUE, trim_ws = TRUE) %>%
-  		rename(Kaiju_class = 'superkingdom') %>%
-  		mutate(Kaiju_class  = ifelse(Kaiju_class  == "Eukaryota", "EUK", "OTHER"))
+	} else{
+		kaiju_class <- read_delim(kaiju_classFile, delim = "\t", escape_double = FALSE, col_names = TRUE, trim_ws = TRUE) %>%
+	  		rename(Kaiju_class = 'superkingdom') %>%
+	  		mutate(Kaiju_class  = ifelse(Kaiju_class  == "Eukaryota", "EUK", "OTHER"))
+	}
+} else if (tolower(refdb_classifier) == "cat"){
+	cat_class <- read_delim(cat_classFile, delim = "\t", col_names = TRUE) %>%
+		rename(ContigID = `# contig`) %>%
+		mutate(CAT_class = ifelse(str_detect(superkingdom, "Eukaryota"), "EUK", "OTHER")) 
 }
 
 
@@ -66,19 +74,36 @@ contigs_class <- contigs_class %>% mutate(
   MajorityKmer_class = ifelse(MajorityKmer_votesFrac > 0.5, "EUK", "OTHER")) 
 
 
-##### KAIJU CLASSIFICATION #####
+##### REFERENCE-BASED CLASSIFICATION #####
 
-contigs_class <- kaiju_class %>% select(ContigID, Kaiju_class) %>% right_join(contigs_class, by = "ContigID")
-contigs_class <- contigs_class %>% 
-  mutate(MajorityKmer_Kaiju_class = ifelse(is.na(Kaiju_class), MajorityKmer_class, Kaiju_class))
-write_delim(contigs_class, paste0(out_folder, results_filename, ".Classification_details_Kmer_min",kmer_minLength,"bp_Kaiju_min",kaiju_minLength,"bp.tsv"), delim = "\t")
-
-if (include_na == "TRUE"){
-	EUKs_ContigID <- contigs_class %>% filter(MajorityKmer_Kaiju_class != "OTHER") %>% select(ContigID)
-	write_delim(EUKs_ContigID, paste0(out_folder, results_filename, ".EUK_NA_contigsIDs_Kmer_min",kmer_minLength,"bp_Kaiju_min",kaiju_minLength,"bp.txt"), col_names = FALSE)
-} else if (include_na == "FALSE") {
-	EUKs_ContigID <- contigs_class %>% filter(MajorityKmer_Kaiju_class == "EUK") %>% select(ContigID)
-	write_delim(EUKs_ContigID, paste0(out_folder, results_filename, ".EUK_contigsIDs_Kmer_min",kmer_minLength,"bp_Kaiju_min",kaiju_minLength,"bp.txt"), col_names = FALSE)
-} else {
-	fprintf("Specify if the output should include only the detected eukaryotes or also all the not classified contigs\n")
+if (tolower(refdb_classifier) == "kaiju"){
+	contigs_class <- kaiju_class %>% select(ContigID, Kaiju_class) %>% right_join(contigs_class, by = "ContigID")
+	contigs_class <- contigs_class %>% 
+	  mutate(MajorityKmer_Kaiju_class = ifelse(is.na(Kaiju_class), MajorityKmer_class, Kaiju_class))
+	write_delim(contigs_class, paste0(out_folder, results_filename, ".Classification_details_KmerMajority_min",kmer_minLength,"bp_KAIJU_min",refdb_minLength,"bp.tsv"), delim = "\t")
+	
+	if (tolower(include_na) == "true"){
+		EUKs_ContigID <- contigs_class %>% filter(MajorityKmer_Kaiju_class != "OTHER") %>% select(ContigID)
+		write_delim(EUKs_ContigID, paste0(out_folder, results_filename, ".EUK_NA_contigsIDs_KmerMajority_min",kmer_minLength,"bp_KAIJU_min",refdb_minLength,"bp.txt"), col_names = FALSE)
+	} else if (tolower(include_na) == "false") {
+		EUKs_ContigID <- contigs_class %>% filter(MajorityKmer_Kaiju_class == "EUK") %>% select(ContigID)
+		write_delim(EUKs_ContigID, paste0(out_folder, results_filename, ".EUK_contigsIDs_KmerMajority_min",kmer_minLength,"bp_KAIJU_min",refdb_minLength,"bp.txt"), col_names = FALSE)
+	} else {
+		fprintf("Specify if the output should include only the detected eukaryotes or also all the not classified contigs\n")
+	}
+} else if (tolower(refdb_classifier) == "cat"){
+	contigs_class <- cat_class %>% select(ContigID, CAT_class) %>% right_join(contigs_class, by = "ContigID")
+	contigs_class <- contigs_class %>% 
+	  mutate(MajorityKmer_CAT_class = ifelse(is.na(CAT_class), MajorityKmer_class, CAT_class))
+	write_delim(contigs_class, paste0(out_folder, results_filename, ".Classification_details_KmerMajority_min",kmer_minLength,"bp_CAT_min",refdb_minLength,"bp.tsv"), delim = "\t")
+	
+	if (tolower(include_na) == "true"){
+		EUKs_ContigID <- contigs_class %>% filter(MajorityKmer_CAT_class != "OTHER") %>% select(ContigID)
+		write_delim(EUKs_ContigID, paste0(out_folder, results_filename, ".EUK_NA_contigsIDs_KmerMajority_min",kmer_minLength,"bp_CAT_min",refdb_minLength,"bp.txt"), col_names = FALSE)
+	} else if (tolower(include_na) == "false") {
+		EUKs_ContigID <- contigs_class %>% filter(MajorityKmer_CAT_class == "EUK") %>% select(ContigID)
+		write_delim(EUKs_ContigID, paste0(out_folder, results_filename, ".EUK_contigsIDs_KmerMajority_min",kmer_minLength,"bp_CAT_min",refdb_minLength,"bp.txt"), col_names = FALSE)
+	} else {
+		fprintf("Specify if the output should include only the detected eukaryotes or also all the not classified contigs\n")
+	}
 }
